22c2cc26bf1492d5ef0f6d6d5dc79141fda77141
Ibrahim OGUZ <ibrahimoguz.ie@gmail.com>
2018-03-21 16:17:03 +0200

    Server and Client components added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
